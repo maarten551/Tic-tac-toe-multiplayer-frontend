f97580c967028976b84138c2eb3752e9222f29fd
Maarten Bobbeldijk <maarten@bobbeldijk.nl>
2019-10-22 17:02:56 +0200

    Added first working version with websockets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
+import {RxStompService} from '@stomp/ng2-stompjs';
+import {Message} from '@stomp/stompjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tic-tac-toe-front-end';
+  private title = 'tic-tac-toe-front-end';
+  private messages: string[] = [];
+  private inputChatMessage = '';
+  private inputName = '';
+
+  constructor(private rxStompService: RxStompService) {
+    this.initializeWebSocketConnection();
+  }
+
+  private initializeWebSocketConnection(): void {
+    this.rxStompService.watch('/chat').subscribe((message: Message) => {
+      this.messages.push(message.body);
+    });
+  }
+
+  private sendChatMessage(): void {
+    this.rxStompService.publish({destination: '/app/send/message', body: this.inputChatMessage, headers: {}});
+    this.inputChatMessage = '';
+  }
+
+  private sendUsername(): void {
+    this.rxStompService.publish({destination: '/app/send/username', body: this.inputName, headers: {}});
+    this.inputName = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {InjectableRxStompConfig, RxStompService, rxStompServiceFactory} from '@stomp/ng2-stompjs';
+import { myRxStompConfig } from './my-rx-stomp.config';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+  providers: [
+    {
+      provide: InjectableRxStompConfig,
+      useValue: myRxStompConfig
+    },
+    {
+      provide: RxStompService,
+      useFactory: rxStompServiceFactory,
+      deps: [InjectableRxStompConfig]
+    }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
